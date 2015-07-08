eab5f09b1338781c02f88245ff91ad049ac3a017
mtbvang <mtb.vang@gmail.com>
2015-07-08 23:03:29 +0200

    Removed sudo from readme added extra token substitutions for vagrantfile template file updates that make it not reliant on devops starter.

--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 A quick way to add devops related tooling to projects. Get the setup file for the kind of project you are starting. For example for node projects:
 
 ```sh
-sudo curl -O https://raw.githubusercontent.com/mtbvang/devops-starter/master/setup-node.sh
-sudo chmod +x setup-node.sh
-sudo ./setup-node.sh <projectname> <portoffset>
+curl -O https://raw.githubusercontent.com/mtbvang/devops-starter/master/setup-node.sh
+chmod +x setup-node.sh
+./setup-node.sh <projectname> <portoffset>
 ```
 
 './setup-node.sh test-app 1' will result in a node application running on port 1338. Look at the setup-node.sh file for details of other arguments.
--- setup-node.sh
+++ setup-node.sh
@@ -11,6 +11,7 @@ trim() {
 init() {
 	PROJECT_ROOT=$(cd `dirname ${0}`; pwd)
 	DEVOPS_DIR_NAME=devops
+	DEVOPS_DIR=${DEVOPS_DIR_NAME}/devops
 }
 
 # Clone devops-starter as a submodule.
@@ -29,9 +30,9 @@ setupVagrantFile() {
 	sed -i "s/<projectName>/${PROJECT_NAME}/g" Vagrantfile
 	sed -i "s/<portOffset>/${PORT_OFFSET}/g" Vagrantfile
 	sed -i "s/<guestAppPort>/${GUEST_APP_PORT}/g" Vagrantfile
-	
-	VAGRANT_STARTER_DIR="${DEVOPS_DIR_NAME}/vagrant"
-	sed -i "s+<vagrantStarterDir>+${VAGRANT_STARTER_DIR}+g" Vagrantfile
+	sed -i "s+<dotfilesDir>+${DEVOPS_DIR}/dotfiles+g" Vagrantfile
+	sed -i "s+<vagrantDir>+${DEVOPS_DIR}/vagrant+g" Vagrantfile
+	sed -i "s+<provisioningDir>+${DEVOPS_DIR}/provisioning+g" Vagrantfile
 }
 
 initGit() {
